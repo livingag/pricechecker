99a3ba9a9caa94af97f41b3a823ca5d180c66ddf
livingag <livingstone.alex@gmail.com>
2025-07-23 11:39:03 +1000

    get products config from URL

--- README.md
+++ README.md
@@ -3,11 +3,23 @@ Checks whether products are on sale at Woolworths or Coles
 
 ## Installation
 
-Create a virtual environment using `requirements.txt` and just run the script! It will scrape the Woolworths and Coles APIs for prices of products contained in the `products.yaml` file and notify you of the specials through an email. Schedule the script to be run every Wednesday (when the specials get changed) to be notified if any of your configured products are on sale for the week.
+Create a virtual environment using `requirements.txt` and just run the script! It will scrape the Woolworths and Coles APIs for prices of products contained in the `products.yaml` file and notify you of the specials through an email.
+
+Schedule the script to be run every Wednesday (when the specials get changed) to be notified if any of your configured products are on sale for the week.
 
 ## Configuration
 
-Edit the `GMAIL_ADDRESS` and `GMAIL_PASSWORD` variables in `pricechecker.py` in order to use email notifications.
+A `config.ini` file is required with the following format:
+
+```
+[Gmail]
+address = your.email@gmail.com
+password = your_app_password
+
+[Products]
+url = URL of products.yaml file
+```
+
 
 `products.yaml` shows an example product configuration. The product IDs are visible in both the Woolworths and Coles product page URLs:
 
@@ -16,3 +28,5 @@ https://www.woolworths.com.au/shop/productdetails/<b>36066</b>/arnott-s-tim-tam-
 
 https://www.coles.com.au/product/arnotts-tim-tam-chocolate-biscuits-original-200g-<b>329607</b>
 </pre>
+
+Storing the `products.yaml` as a GitHub Gist is the intended use case.
--- pricechecker.py
+++ pricechecker.py
@@ -1,39 +1,53 @@
-import requests, json, yaml, smtplib
+import requests
+import json
+import yaml
+import smtplib
+import configparser
 from email.mime.text import MIMEText
 
-HEADERS = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-GMAIL_ADDRESS = "address@gmail.com"
-GMAIL_PASSWORD = "password_here"
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+}
+GMAIL_ADDRESS = config.get("Gmail", "address")
+GMAIL_PASSWORD = config.get("Gmail", "password")
+PRODUCTS_URL = config.get("Products", "url")
 
 WOOLIES_URL = "https://www.woolworths.com.au/apis/ui/products/"
 COLES_URL = "https://www.coles.com.au/api/products"
 
+
 def get_coles_products(product_ids):
-    data = {"productIds":",".join(product_ids)}
+    data = {"productIds": ",".join(product_ids)}
 
     r = requests.get(COLES_URL, headers=HEADERS, json=data)
 
     return json.loads(r.text)["results"]
 
-def get_woolies_products(product_ids):
 
+def get_woolies_products(product_ids):
     r = requests.get(WOOLIES_URL + ",".join(product_ids), headers=HEADERS)
 
     return json.loads(r.text)
 
+
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = ", ".join(recipients)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
     print("Email sent!")
 
 
-with open("products.yaml") as stream:
-    product_ids = yaml.safe_load(stream)
+response = requests.get(PRODUCTS_URL)
+content = response.content.decode("utf-8")
+product_ids = yaml.safe_load(content)
 
 woolies_ids = [str(v["woolies"]) for v in product_ids.values() if v["woolies"] is not None]
 coles_ids = [str(v["coles"]) for v in product_ids.values() if v["coles"] is not None]
@@ -43,7 +57,7 @@ woolies_specials = []
 for product in products:
     if product["IsOnSpecial"]:
         saving = (product["SavingsAmount"] / product["WasPrice"]) * 100
-        woolies_specials.append(f'{product["DisplayName"]}: ${product["Price"]:.2f} ({saving:.0f}% off)\n')
+        woolies_specials.append(f"{product['DisplayName']}: ${product['Price']:.2f} ({saving:.0f}% off)\n")
 woolies_specials = "".join(sorted(woolies_specials))
 
 products = get_coles_products(coles_ids)
@@ -51,9 +65,9 @@ coles_specials = []
 for product in products:
     pricing = product["pricing"]
     if pricing["was"] != 0:
-        name = f'{product["brand"]} {product["name"]} {product["size"]}'
+        name = f"{product['brand']} {product['name']} {product['size']}"
         saving = (pricing["saveAmount"] / pricing["was"]) * 100
-        coles_specials.append(f'{name}: ${pricing["now"]:.2f} ({saving:.0f}% off)\n')
+        coles_specials.append(f"{name}: ${pricing['now']:.2f} ({saving:.0f}% off)\n")
 coles_specials = "".join(sorted(coles_specials))
 
 if len(woolies_specials + coles_specials) > 0:
